1b7c9cc321e92989c0358bcc2d3746df815a4521
Thiago Wesley Cunha <thiagotwc@gmail.com>
2026-04-14 21:48:21 -0300

    Get And Set Criados e função

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,12 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author 1091809
- */
 public class Tarefa {
     
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String titulo, String descricao, boolean concluida) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.concluida = concluida;
+    }
+    
+    public void marcarComoConcluida(){
+        
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
 }
