d5770255b7260c0c3368a381f2ffc343a28a0f17
joaovictorcastro178 <joaovictorcastro178@gamil.com>
2026-04-15 21:49:23 -0300

    menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,16 @@
+
+import java.util.Scanner;
+
 public class Menu {
     
+    public static int menu(Scanner scanner){
+        System.out.println("Escolha um opção");
+        System.out.println("[1] Criar tarefa ");
+        System.out.println("[2] Listar tarefas ");
+        System.out.println("[3] Marcar tarefa como conlcuida ");
+        System.out.println("[4] Exibir status da tarefa ");
+        System.out.println("[5] Sair (Encerrar Sistema) ");
+        
+        int opcao= scanner.nextInt();
+    }
 }
