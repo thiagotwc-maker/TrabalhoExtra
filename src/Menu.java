34d5b6285d730590c017726a88047b610233bd4d
Joao Pedro de Souza Santos <1094296@senacminas.edu.br>
2026-04-17 19:47:21 -0300

    Swicht Case

--- src/Menu.java
+++ src/Menu.java
@@ -12,5 +12,33 @@ public class Menu {
         System.out.println("[5] Sair (Encerrar Sistema) ");
         
         int opcao= scanner.nextInt();
+        
+        // Limpar
+        scanner.nextLine();
+        
+        //Se for necessario
+        //return opcao;
+        
+        switch (opcao) {
+            case 1:
+                adicionarTarefa();
+                break;
+                
+            case 2:
+                gerenciador.listarTarefa();
+                break;
+                
+            case 3:
+                concluirTarefa();
+                break;
+                
+            case 4:
+                //criar função exibir status da tarefa
+                break;
+                
+            case 5:
+                System.out.println("Saindo");
+                break;
+        }
     }
 }
