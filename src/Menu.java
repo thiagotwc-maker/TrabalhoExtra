55b97e63622807476c0f3ac64e3e21e73c525e51
joaovictorcastro178 <joaovictorcastro178@gamil.com>
2026-04-29 20:20:56 -0300

    switch case 2 e 3

--- src/Menu.java
+++ src/Menu.java
@@ -15,31 +15,31 @@ public class Menu {
         System.out.println("[3] Marcar tarefa como concluida ");
         System.out.println("[4] Sair (Encerrar Sistema) ");
         
-        int opcao= scanner.nextInt();
-        
-        // Limpar
-        scanner.nextLine();
-        
-        //Se for necessario
-        return opcao;
-        
-        switch (opcao){
-            case 1:Tarefa tarefa =  new Tarefa();
-                funcao.adicionarTarefa(tarefa);
-                break;
-                
-            case 2:
-                funcao.listarTarefas();
-                break;
-                
-            case 3:
-                funcao.concluirTarefa(int indice);
-                break;
-                
-                
-            case 4:
-                System.out.println("Saindo");
-                break;
+        opcao = scanner.nextInt();
+            scanner.nextLine(); // limpar buffer
+ 
+            switch (opcao) {
+                case 1:
+                    System.out.print("Título: ");
+                    String titulo = scanner.nextLine();
+ 
+                    System.out.print("Descrição: ");
+                    String descricao = scanner.nextLine();
+ 
+                    Tarefa tarefa = new Tarefa(titulo, descricao);
+                    gerenciador.adicionarTarefa(tarefa);
+                    break;
+        //atualizado case 1.0 - JV
+                case 2:
+                    gerenciador.listarTarefas();
+                    break;
+ 
+                case 3:
+                    System.out.print("Digite o índice da tarefa: ");
+                    int indice = scanner.nextInt();
+                    gerenciador.concluirTarefa(indice);
+                    break;
+        //atualizado case 2.0 - JV
         }
     }
 }
