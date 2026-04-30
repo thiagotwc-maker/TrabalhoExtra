1efa339e9876d8b69b9e1809391cd851d2b7336a
Joao Pedro de Souza Santos <1094296@senacminas.edu.br>
2026-04-29 21:52:09 -0300

    Tarefa Refeita JP - JV

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,45 +1,28 @@
 public class Tarefa {
-    
+
     private String titulo;
     private String descricao;
     private boolean concluida;
 
-    public Tarefa(String titulo, String descricao, boolean concluida) {
+    public Tarefa(String titulo, String descricao) {
         this.titulo = titulo;
         this.descricao = descricao;
-        this.concluida = concluida;
+        this.concluida = false;
     }
 
-    public Tarefa() {
+    public void marcarComoConcluida() {
+        this.concluida = true;
     }
-    
-    
-    
-    public void marcarComoConcluida(){
-        
-    }
-    
+
     public String getTitulo() {
         return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public boolean isConcluida() {
         return concluida;
     }
-
-    public void setConcluida(boolean concluida) {
-        this.concluida = concluida;
-    }
-}
+}s
